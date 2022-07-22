d48bc481827306d0e10b5e481cb29cd1a88a69dc
caoyang <yangcao@emotibot.com>
2022-07-22 11:41:47 +0800

    httputil

---
+++ common/src/main/java/com/odd/util/HttpUtil.java
@@ -0,0 +1,38 @@
+package com.odd.util;
+
+import okhttp3.*;
+
+import java.io.IOException;
+
+public class HttpUtil {
+
+    static OkHttpClient client = new OkHttpClient();
+    public static final MediaType JSON
+            = MediaType.get("application/json; charset=utf-8");
+
+    public static String get(String url){
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            return response.body().string();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String post(String url, String params){
+        RequestBody body = RequestBody.create(params, JSON);
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            return response.body().string();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
