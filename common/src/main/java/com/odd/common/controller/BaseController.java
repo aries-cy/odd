3cb35315d357ab8112922dbe252e6913d03d61f0
caoyang <yangcao@emotibot.com>
2023-04-06 22:05:56 +0800

    base类

--- common/src/main/java/com/odd/common/controller/BaseController.java
+++ common/src/main/java/com/odd/common/controller/BaseController.java
@@ -4,7 +4,6 @@ import com.odd.common.service.BaseService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.*;
 import org.springframework.web.bind.annotation.*;
-
 import javax.annotation.Resource;
 import java.util.List;
 import java.util.Optional;
--- common/src/main/java/com/odd/common/service/BaseService.java
+++ common/src/main/java/com/odd/common/service/BaseService.java
@@ -2,14 +2,15 @@ package com.odd.common.service;
 
 import com.odd.common.repository.BaseRepository;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 public class BaseService<R extends BaseRepository<T>, T> {
 
     @Autowired
