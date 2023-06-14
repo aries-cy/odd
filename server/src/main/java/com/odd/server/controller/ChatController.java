5b594df85707418a26d7225109cdb77e43637e0e
caoyang <yangcao@emotibot.com>
2023-06-14 17:00:28 +0800

    添加logback日志

--- server/src/main/java/com/odd/server/controller/ChatController.java
+++ server/src/main/java/com/odd/server/controller/ChatController.java
@@ -4,19 +4,21 @@ import com.alibaba.fastjson.JSONObject;
 import com.odd.common.entity.Message;
 import com.odd.common.util.WxUtil;
 import com.odd.common.util.XmlUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class ChatController {
 
 
     @GetMapping("/wx")
     public String checkToken(String signature, String timestamp, String nonce, String echostr) {
-        System.out.println("signature:" + signature + " timestamp:" + timestamp + " nonce:" + nonce + " echostr:" + echostr);
+        log.info("signature:{}, timestamp:{}, nonce:{}, echostr:{}", signature, timestamp, nonce, echostr);
         if (signature != null && WxUtil.checkSignature(signature, timestamp, nonce)) {
             return echostr;
         }
@@ -25,12 +27,12 @@ public class ChatController {
 
     @PostMapping(value = "/wx", produces = MediaType.APPLICATION_XML_VALUE)
     public String chat(@RequestBody Message message) {
-        System.out.println(JSONObject.toJSONString(message));
+        log.info("user message is : {}", JSONObject.toJSONString(message));
         String fromUserName = message.getFromUserName();
         message.setFromUserName(message.getToUserName());
         message.setToUserName(fromUserName);
         String xml = XmlUtil.objectToXml(message);
-        System.out.println(xml);
+        log.info("to user message is :{}", xml);
         return xml;
     }
 
