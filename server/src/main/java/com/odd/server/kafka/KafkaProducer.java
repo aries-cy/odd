e4ed36e33062d06ed2f2cd9135aed0070b5412a6
caoyang <yangcao@emotibot.com>
2023-07-12 21:14:07 +0800

    kafka

--- server/src/main/java/com/odd/server/kafka/KafkaProducer.java
+++ server/src/main/java/com/odd/server/kafka/KafkaProducer.java
@@ -3,7 +3,11 @@ package com.odd.server.kafka;
 import com.odd.server.common.Constant;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Component;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+
 import javax.annotation.Resource;
 
 @Slf4j
@@ -15,6 +19,17 @@ public class KafkaProducer {
 
 
     public void sendMessage(String message) {
-        kafkaTemplate.send(Constant.TOPIC, message);
+        ListenableFuture<SendResult<String, String>> future = kafkaTemplate.send(Constant.TOPIC, message);
+        future.addCallback(new ListenableFutureCallback<SendResult<String, String>>() {
+            @Override
+            public void onFailure(Throwable ex) {
+                log.info("Send msg failed...");
+            }
+
+            @Override
+            public void onSuccess(SendResult<String, String> result) {
+                log.info("Send msg success...");
+            }
+        });
     }
 }
